6e691a3bfdaf9dfcab4f3adea2d968c580c9a71d
Omer-Cicek <omercicek160408@gmail.com>
2022-06-20 19:09:23 +0300

    first commit

---
+++ src/components/Result.vue
@@ -0,0 +1,13 @@
+<template>
+  <p>counter is: {{ counter }}</p>
+</template>
+
+<script>
+export default {
+  computed: {
+    counter() {
+      return this.$store.getters.doubleCounter;
+    },
+  },
+};
+</script>
